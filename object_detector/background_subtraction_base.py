ed32b4e092dce8e5ad3a8d9d633416de480b13ef
MichaelSkryl <90468326+MichaelSkryl@users.noreply.github.com>
2023-11-17 17:25:28 +0300

    Mistakes corrected

--- object_detector/background_subtraction_base.py
+++ object_detector/background_subtraction_base.py
@@ -2,32 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class BackgroundSubtractor(ABC):
-    def __init__(self):
-        self.is_inited = False
-
-    def get_init_flag(self):
-        return self.is_inited
-
-    def reset(self):
-        if self.get_init_flag():
-            self.areset()
-        else:
-            return -1       # Возможно, здесь необходимо исключение?
-
-    def apply(self, image):
-        if self.get_init_flag():
-            self.apply(image)
-        else:
-            return -1
-
-    @abstractmethod
-    def init(self):
-        pass
-
-    @abstractmethod
-    def areset(self):
-        pass
-
+   
     @abstractmethod
     def set_params(self, **params):
         pass
