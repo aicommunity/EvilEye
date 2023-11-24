e0e519f88afc0e575dbaf4beea279e4d0d2154f6
MichaelSkryl <90468326+MichaelSkryl@users.noreply.github.com>
2023-11-24 11:48:18 +0300

    Added base class for all modules

---
+++ core/base_class.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+
+class BaseClass(ABC):
+    def __init__(self):
+        self.is_inited = False
+        self.params = {}
+
+    def set_params(self, **params):
+        self.params = params
+        self.set_params_impl()
+
+    def get_init_flag(self):
+        return self.is_inited
+
+    def reset(self):
+        if self.get_init_flag():
+            self.reset_impl()
+        else:
+            raise Exception('init function has not been called')
+
+    def init(self):
+        self.is_inited = self.init_impl()
+
+    def get_params(self):
+        return self.params
+
+    @abstractmethod
+    def default(self):
+        pass
+
+    @abstractmethod
+    def init_impl(self):
+        pass
+
+    @abstractmethod
+    def reset_impl(self):
+        pass
+
+    @abstractmethod
+    def set_params_impl(self):
+        pass
