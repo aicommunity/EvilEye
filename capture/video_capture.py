34f7bf1da12a7e25eccf3553d105ff94a448a236
MichaelSkryl <90468326+MichaelSkryl@users.noreply.github.com>
2023-12-15 15:08:23 +0300

    Added stream split

--- capture/video_capture.py
+++ capture/video_capture.py
@@ -15,9 +15,17 @@ class VideoCapture(capture.VideoCaptureBase):
         else:
             pass
 
-    def process_impl(self):
-        is_read, image = self.capture.read()
-        return is_read, image
+    def process_impl(self, split_stream=False, num_split=None, roi=None):
+        is_read, src_image = self.capture.read()
+        if split_stream:
+            streams = []
+            streams.append(src_image[0:int(roi[0][1]), 0:int(roi[0][2])].copy())
+            for stream_cnt in range(num_split - 1):
+                streams.append(src_image[roi[stream_cnt][1]:roi[stream_cnt][1]+int(roi[stream_cnt][3]),
+                                         roi[stream_cnt][0]:roi[stream_cnt][0]+int(roi[stream_cnt][2])].copy())
+            return is_read, streams
+        else:
+            return is_read, src_image
 
     def default(self):
         self.params.clear()
--- capture/video_capture_base.py
+++ capture/video_capture_base.py
@@ -2,25 +2,44 @@ from abc import ABC, abstractmethod
 import numpy as np
 import cv2
 import core
+from enum import IntEnum
 
 
 class VideoCaptureBase(core.EvilEyeBase):
+    source_count = 0
+    video_sources = []
+
+    class VideoCaptureAPIs(IntEnum):
+        CAP_ANY = 0
+        CAP_GSTREAMER = 1800
+        CAP_FFMPEG = 1900
+        CAP_IMAGES = 2000
+
     def __init__(self):
         super().__init__()
         self.capture = cv2.VideoCapture()
+        self.stream_idx = VideoCaptureBase.source_count
+        VideoCaptureBase.video_sources.append(self.capture)
+        VideoCaptureBase.source_count += 1
 
     def is_opened(self):
         return self.capture.isOpened()
 
     def release(self):
+        VideoCaptureBase.source_count -= 1
+        del VideoCaptureBase.video_sources[self.stream_idx]
         self.capture.release()
 
-    def process(self):
+    @classmethod
+    def get_src_by_index(cls, index):
+        return cls.video_sources[index]
+
+    def process(self, split_stream=False, num_split=None, roi=None):
         if self.get_init_flag():
-            return self.process_impl()
+            return self.process_impl(split_stream, num_split, roi)
         else:
             raise Exception('init function has not been called')
 
     @abstractmethod
-    def process_impl(self):
+    def process_impl(self, split_stream=False, num_split=None, roi=None):
         pass
