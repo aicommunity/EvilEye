10b44117a7989c253390196f009c7d21885c7024
MichaelSkryl <90468326+MichaelSkryl@users.noreply.github.com>
2024-01-31 13:33:20 +0300

    Added roi creation and boxes merging

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 def boxes_iou(box1, box2):
-    if (((box1[0] <= box2[0] and box1[1] <= box2[1]) and (box2[2] <= box1[2] and box2[3] <= box1[3])) or  # Находится ли один bbox внутри другого
+    if (((box1[0] <= box2[0] and box1[1] <= box2[1]) and (
+            box2[2] <= box1[2] and box2[3] <= box1[3])) or  # Находится ли один bbox внутри другого
             ((box2[0] <= box1[0] and box2[1] <= box1[1]) and (box1[2] <= box2[2] and box1[3] <= box2[3]))):
         return 1.0
     x_left = max(box1[0], box2[0])
@@ -32,7 +33,7 @@ def non_max_sup(boxes_coords, confidences, class_ids):
         for i in range(len(sorted_idxs) - 1):
             idx = sorted_idxs[i]
             iou = boxes_iou(boxes_coords[sorted_idxs[last]], boxes_coords[idx])
-            if iou > iou_thresh: # Если iou превышает порог, то добавляем данную рамку на удаление
+            if iou > iou_thresh:  # Если iou превышает порог, то добавляем данную рамку на удаление
                 suppress_idxs.append(i)
         sorted_idxs = np.delete(sorted_idxs, suppress_idxs)
     boxes_coords = boxes_coords[keep_idxs].tolist()
@@ -45,3 +46,43 @@ def roi_to_image(roi_box_coords, x0, y0):
     image_box_coords = [x0 + int(roi_box_coords[0]), y0 + int(roi_box_coords[1]),
                         x0 + int(roi_box_coords[2]), y0 + int(roi_box_coords[3])]
     return image_box_coords
+
+
+def create_roi(image, coords):
+    rois = []
+    for count in range(len(coords)):
+        rois.append([image[coords[count][1]:coords[count][1] + coords[count][3],
+                     coords[count][0]:coords[count][0] + coords[count][2]], [coords[count][0], coords[count][1]]])
+    return rois
+
+
+def merge_roi_boxes(all_roi, bboxes_coords, confidences, class_ids):
+    for i in range(len(list(bboxes_coords))):
+        for j in range(i + 1, len(list(bboxes_coords))):
+            # Если рамки пересекаются, но находятся в разных регионах, то объединяем их в одну рамку
+            if is_intersected(bboxes_coords[i], bboxes_coords[j]) and not is_same_roi(all_roi, bboxes_coords[i],
+                                                                                      bboxes_coords[j]):
+                bboxes_coords[i] = [min(bboxes_coords[i][0], bboxes_coords[j][0]),
+                                    min(bboxes_coords[i][1], bboxes_coords[j][1]),
+                                    max(bboxes_coords[i][2], bboxes_coords[j][2]),
+                                    max(bboxes_coords[i][3], bboxes_coords[j][3])]
+                confidences[i] = max(confidences[i], confidences[j])
+                del bboxes_coords[j]
+                del confidences[j]
+                del class_ids[j]
+    return bboxes_coords, confidences, class_ids
+
+
+def is_same_roi(all_roi, box1, box2):
+    for roi in all_roi:
+        if ((roi[1] <= box1[3] <= (roi[1] + roi[3]) and roi[1] <= box1[1] <= (roi[1] + roi[3])) and
+                (roi[1] <= box2[3] <= (roi[1] + roi[3]) and roi[1] <= box2[1] <= (roi[1] + roi[3]))):
+            return True
+    return False
+
+
+def is_intersected(box1, box2):
+    if ((box1[2] >= box2[0]) and (box2[2] >= box1[0])) and ((box1[3] >= box2[1]) and (box2[3] >= box1[1])):
+        return True
+    else:
+        return False
