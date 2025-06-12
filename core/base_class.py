c65c0a1182f00f738e4e8a439ecb7dd9fe5d4876
Alexander Bakhshiev <palexab@gmail.com>
2025-06-12 14:37:02 +0300

    Add simple memory leak control

--- core/base_class.py
+++ core/base_class.py
@@ -1,11 +1,17 @@
 from abc import ABC, abstractmethod
+from pympler import asizeof
+import datetime
 
 
 class EvilEyeBase(ABC):
+    id_counter = 0
     def __init__(self):
         self.is_inited = False
-        self.id: int = 0
+        self.id: int = EvilEyeBase.id_counter
+        EvilEyeBase.id_counter += 1
         self.params = {}
+        self.memory_measure_results = None
+        self.memory_measure_time = None
 
     def set_params(self, **params):
         self.params = params
@@ -35,8 +41,25 @@ class EvilEyeBase(ABC):
     def get_params(self):
         return self.params
 
-    def get_debug_info(self, debug_info: dict):
+    def get_debug_info(self, debug_info: dict | None):
+        if debug_info is None:
+            debug_info = dict()
+        debug_info['id'] = self.id
         debug_info['is_inited'] = self.is_inited
+        debug_info['memory_measure_results'] = self.memory_measure_results
+        debug_info['memory_measure_time'] = self.memory_measure_time
+
+    def insert_debug_info_by_id(self, debug_info: dict | None):
+        if debug_info is None:
+            debug_info = dict()
+        comp_debug_info = debug_info[self.id] = dict()
+        self.get_debug_info(comp_debug_info)
+        return debug_info[self.id]
+
+    def calc_memory_consumption(self):
+        self.memory_measure_results = asizeof.asizeof(self)
+        self.memory_measure_time = datetime.datetime.now()
+
 
     @abstractmethod
     def default(self):
