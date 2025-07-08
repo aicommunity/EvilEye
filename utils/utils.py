95fbdb0ec74851be781c84d1e550b02cc714432c
Alexander Bakhshiev <palexab@gmail.com>
2025-07-08 20:10:11 +0300

    Improve visualization in reid mode

--- utils/utils.py
+++ utils/utils.py
@@ -272,7 +272,7 @@ def draw_boxes_tracking(image: CaptureImage, cameras_objs, source_name, source_d
                       (int(last_info.bounding_box[2]), int(last_info.bounding_box[3])), (0, 255, 0), thickness=font_thickness)
         if obj.global_id is not None:
             cv2.putText(image.image,
-                        str(last_info.track_id) + ':' + str(obj.global_id) + ' ' + str([last_info.class_id]) +
+                        'g' + str(obj.global_id) + ' ' + str([last_info.class_id]) +
                         " " + "{:.2f}".format(last_info.confidence),
                         (int(last_info.bounding_box[0]), int(last_info.bounding_box[1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
                         font_scale, font_color, font_thickness)
